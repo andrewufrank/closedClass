832d4ea3d74b9671efcc2c05cea6cca4ba227b6c
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2018-03-03 10:30:18 +0100

    coref without labels -- not working

--- Lib/ParseJsonCoreNLP.hs
+++ Lib/ParseJsonCoreNLP.hs
@@ -23,6 +23,7 @@ import Data.Aeson.Types  -- for modifying the labels
 import GHC.Generics
 --import qualified Data.ByteString.Lazy as B
 import Control.Monad (mzero)
+import qualified Data.HashMap.Strict as HM
 
 parseNLP :: ErrIO ()
 parseNLP = do
@@ -34,13 +35,13 @@ parseNLP = do
 decodeDoc1 :: LazyByteString -> Maybe Doc1
 decodeDoc1 = decode
 
-data Doc1 = Doc1 {sentences::  [Sentence1]
---                 , doc_corefs :: [CorefChain1]
+data Doc1 = Doc1 {doc_sentences::  [Sentence1]
+                  , doc_corefs :: [CorefChain1]
                        } deriving (Read, Show,  Eq, Ord, Generic)
 
 instance FromJSON Doc1 where
---    parseJSON = genericParseJSON defaultOptions {
---                fieldLabelModifier = drop 4 }
+    parseJSON = genericParseJSON defaultOptions {
+                fieldLabelModifier = drop 4 }
 
 data Sentence1 = Sentence1 {s_index :: Int
                         , s_parse :: Text  -- the parse tree
@@ -97,31 +98,37 @@ instance FromJSON Token1 where
     parseJSON = genericParseJSON defaultOptions {
                 fieldLabelModifier = drop 4 }
 
-data CorefChain1 = CorefChain1 {corefs :: [Coref1]
+data CorefChain1 = CorefChain1 {chain_corefs :: [Coref1]
                 } deriving (Read, Show,  Eq, Ord, Generic)
 
 instance FromJSON CorefChain1 where
-    parseJSON = genericParseJSON defaultOptions {
-                fieldLabelModifier = drop 6 }
+    parseJSON =   genericParseJSON opts . jsonLower
+        where
+          opts = defaultOptions { fieldLabelModifier =  drop 6 }
+
+-- | Turn all keys in a JSON object to "chain"
+jsonLower :: Value -> Value
+jsonLower (Object o) = Object . HM.fromList . map lowerPair . HM.toList $ o
+  where lowerPair (key, val) = ("chain", val)
+jsonLower x = x
 
 data Coref1 = Coref1 {coref_id :: Int
-                    , coref_text :: Text
-                    , coref_type :: Text
-                    , coref_number :: Text
-                    , coref_gender :: Text
-                    , coref_animacy :: Text
-                    , coref_startIndex :: Int
-                    , coref_endIndex :: Int
-                    , coref_headIndex :: Int
-                    , coref_sentNum :: Int
-                    , coref_position :: [Int]
+--                    , coref_text :: Text
+--                    , coref_type :: Text
+--                    , coref_number :: Text
+--                    , coref_gender :: Text
+--                    , coref_animacy :: Text
+--                    , coref_startIndex :: Int
+--                    , coref_endIndex :: Int
+--                    , coref_headIndex :: Int
+--                    , coref_sentNum :: Int
+--                    , coref_position :: [Int]
                     , coref_isRepresentativeMention :: Bool
                 } deriving (Read, Show,  Eq, Ord, Generic)
 
 instance FromJSON Coref1 where
-    parseJSON = genericParseJSON defaultOptions {
-                fieldLabelModifier = drop 6 }
-
-
+    parseJSON =   genericParseJSON opts
+        where
+          opts = defaultOptions { fieldLabelModifier =  drop 6 }
 
 --
