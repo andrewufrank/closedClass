f9c315fd142a50acea73711bcbc1832a4164739c
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2018-04-10 17:08:40 +0200

    empty example with zero

--- Lib/FirstSub.hs
+++ Lib/FirstSub.hs
@@ -1,7 +1,6 @@
 -----------------------------------------------------------------------------
 --
--- Module      :   a test for HTF framework
--- insert {-@ HTF_TESTS @-} for each import
+-- Module      :  testing Monoid deriving
 -----------------------------------------------------------------------------
 --{-# OPTIONS_GHC -F -pgmF htfpp #-}
 {-# LANGUAGE FlexibleContexts      #-}
@@ -19,6 +18,19 @@ import Data.Text as T
 import Data.Text.IO as T
 
 import Control.Monad.Error  -- is monads-tf
+
+import Uniform.Zero
+
+class (Monoid l, Zeros (LF l)) => ListForms l   where
+    type LF l
+    prependOne  :: (LF l) -> l   -> l
+    appendOne :: l   -> (LF l) -> l
+    mkOne :: (LF l) -> l
+    appendTwo  :: l  -> l   -> l
+
+    prependOne a la = appendTwo  (mkOne a) la
+    appendOne la a = appendTwo la (mkOne a)
+    appendTwo = (<>)
 
 firstMain :: IO ()
 firstMain = return ()
