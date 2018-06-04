6d646acb71ebff738d3a6ba837dfce5da08221be
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2018-06-04 19:31:14 +0200

    belnap in separate module - seems to work

--- Lib/Belnap.hs
+++ Lib/Belnap.hs
@@ -15,19 +15,33 @@
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE DeriveAnyClass     #-}
 
-module Distinction
+module Belnap
     where
 
 import Test.Framework
 import Uniform.Strings
 import Uniform.Error
 --import GHC.Generic
+import  Data.PartialOrd (PartialOrd, partialLEQ, partialEQ, partialLT, partialGT)
 
-import Belnap
 
-class Distinctions d where
-    sub, super :: d -> d -> Bool
---    order :: d -> d -> Ordering  -- use compare from Ord
-    join, meet :: d -> d -> d
+data BelnapLogic = Aff | Rej |  Top | Bot deriving (Eq, Show, Read, Enum, Bounded)
 
+        -- affirmativ, reject = negative, indiffirent = not apply, contradiction
+
+-- not4 :: BelnapLogic -> BelnapLogic
+instance  Arbitrary BelnapLogic where
+    arbitrary = arbitraryBoundedEnum
+
+
+instance PartialOrd BelnapLogic where
+    partialLEQ _ Top = True
+    partialLEQ  Bot _  = True
+    partialLEQ _ _ = False
+
+prop_eq :: BelnapLogic -> BelnapLogic -> Bool
+prop_eq a b = (partialEQ a b) == (partialEQ b a )
+
+prop_lt_gt :: BelnapLogic -> BelnapLogic -> Bool
+prop_lt_gt a b = (partialLT a b) == ( (partialGT b a))
 
