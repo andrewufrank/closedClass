fc0dcb68e33502b63b748c55e3fd9b1e87011baf
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2018-06-06 15:46:22 +0200

    cleaned taxon new arbitrary all join/merge tests ok

--- Lib/Taxon.hs
+++ Lib/Taxon.hs
@@ -13,7 +13,6 @@
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeSynonymInstances  #-}
 {-# LANGUAGE DeriveAnyClass     #-}
---{-# LANGUAGE OverlappingInstances     #-}
 
 module Taxon
     where
@@ -21,7 +20,6 @@ module Taxon
 import Test.Framework
 import Uniform.Strings
 import Uniform.Error
---import GHC.Generic
 
 import Distinction
 import qualified Data.Map as Map
@@ -37,37 +35,18 @@ class Taxons t where
 
 instance Taxons Taxon where
     specialize d v t = Taxon . Map.insert d v . unTaxon $ t
---        if isBottom t then errorT ["specialize for bottom not possible"
---                                            , showT d, showT v, showT t]
---                      else dvList2taxon l2
---
---          where l1 = taxon2dvList "specialize" t :: [DistValue]
---                l2 = (DV d v) : l1
-                -- order is not relevant, is produced when extracted from map
-
---    showTaxon :: Taxon -> Text
-    showTaxon t =
-    --        if isBottom t then "Bottom"
-    --                else
-                    s2t . unwords . map showDV
-    --                            . filter (not . isTop . v)
+    showTaxon t =  s2t . unwords . map showDV
                                 . taxon2dvList  $ t
-    -- does not handle bottom?
 
 dvList2taxon :: [DistValue] -> Taxon
 dvList2taxon   =  Taxon . Map.fromList .   (map  dv2pair)
---            . normalizeTaxon $ dvs
 
 taxon2dvList :: Taxon -> [DistValue]
-taxon2dvList   =
---                if isBottom t then errorT ["taxon2dvList not for bottom"]
---                        else
-                            map pair2dv . Map.toAscList . unTaxon
---                            . fromJustNote msg $ t
+taxon2dvList   =        map pair2dv . Map.toAscList . unTaxon
 
 instance Lattice Taxon where
---    lcompare2 = compareTaxon
---    lmeet2 = meetTaxon
+--    lcompare = compareTaxon
+    lmeet = meetTaxon
     ljoin = joinTaxon
     top =  dvList2taxon (makeAll None4)-- []
     bottom = dvList2taxon (makeAll Both4)
@@ -76,16 +55,10 @@ instance Lattice Taxon where
 
 makeAll v = map (\d -> DV d v) allDist :: [DistValue]
     where
-            --makeOne  :: B4val -> Distinction -> DistValue
-            --makeOne v d = DV d v
             allDist = [minBound .. maxBound] :: [DistPaper]
 
 joinTaxon :: Taxon -> Taxon -> Taxon
 joinTaxon a b = dvList2taxon l
---    where
---        l1 = map pair2dv . Map.toAscList . fromJustNote "joinTaxon1" $ t1
---        l2 = map pair2dv . Map.toAscList . fromJustNote "joinTaxon2" $ t2
---        l = join33 l1 l2
     where
         aa = taxon2dvList a :: [DistValue]
         bb = taxon2dvList b :: [DistValue]
@@ -103,8 +76,6 @@ t = zipWith ljoin ph ph
 test_x = assertEqual ph t
 
 
---join33 :: [DistValue] -> [DistValue] -> [DistValue]
---join33 l1 l2  = l1 -- zipWith
 
 --test_join1 = assertEqual physObj (joinTaxon physObj physObj)
 --test_join_milk = assertEqual physObj (joinTaxon milk bread)
@@ -116,35 +87,29 @@ prop_comm1x   = prop_comm1
 prop_comm2x :: Taxon -> Taxon -> Bool
 prop_comm2x = prop_comm2
 
---prop_ass1x, prop_ass2x :: Taxon -> Taxon -> Taxon -> Bool
---prop_ass1x = prop_ass1
---prop_ass2x = prop_ass2
+prop_ass1x, prop_ass2x :: Taxon -> Taxon -> Taxon -> Bool
+prop_ass1x = prop_ass1
+prop_ass2x = prop_ass2
 --
---prop_abs1x, prop_abs2x :: Taxon -> Taxon ->  Bool
---prop_abs1x = prop_abs1
---prop_abs2x = prop_abs2
+prop_abs1x, prop_abs2x :: Taxon -> Taxon ->  Bool
+prop_abs1x = prop_abs1
+prop_abs2x = prop_abs2
 --
---prop_id1x, prop_id2x :: Taxon -> Bool
---prop_id1x = prop_id1
---prop_id2x = prop_id2
+prop_id1x, prop_id2x :: Taxon -> Bool
+prop_id1x = prop_id1
+prop_id2x = prop_id2
 --
---prop_ide1x, prop_ide2x :: Taxon -> Bool
---prop_ide1x = prop_ide1
---prop_ide2x = prop_ide2
+prop_ide1x, prop_ide2x :: Taxon -> Bool
+prop_ide1x = prop_ide1
+prop_ide2x = prop_ide2
 --
---prop_compSymx :: Taxon -> Taxon -> Bool
---prop_compSymx = prop_compSym
+prop_compSymx :: Taxon -> Taxon -> Bool
+prop_compSymx = prop_compSym
 
 --compareTaxon :: Taxon -> Taxon -> PartialRel
 --compareTaxon a b = if null a_b then PGT
 --                        else if null b_a then PLT
 --                            else INC
---
-----         if isPrefixOf l1 l2
-----                                    then PGT
-----                                    else if isPrefixOf l2 l1
-----                                        then PLT
-----                                        else INC
 --    where
 --        aa = taxon2dvList a
 --        bb = taxon2dvList b
@@ -191,27 +156,12 @@ solid = specialize Liquid False4 stuff
 
 instance LatticeTests Taxon
 
---instance {-# OVERLAPS #-} Arbitrary [DistValue] where
---    -- produce only semi-lattice, no bottom
---    arbitrary = do
---        r1 :: [DistValue] <-   arbitrary
-----        let r2 = normalizeTaxon r1
---        return r1
 
---instance Arbitrary Taxon where
---    arbitrary = do
---        d :: [DistValue] <- arbitrary
-----        do
---        return $ dvList2taxon d
---        d :: DistPaper <- arbitraryBoundedEnum
---        v :: B4val <- arbitraryBoundedEnum
---        return (
---instance {-# OVERLAPS #-} Arbitrary Taxon where
----- produces only elements for a semi lattice (no bottom)
---    arbitrary = do
---        r2 ::[DistValue] <- arbitrary -- suchThat arbitrary (not . isNothing)
---        return . dvList2taxon $ r2
-----            . fromJustNote "arbitrary dvList2taxon" $ r2
+instance Arbitrary Taxon where
+    arbitrary = do
+        d :: [DistValue] <- arbitrary
+        let d2 = makeAll None4 ++ d
+        return $ dvList2taxon d2
 
 
 
