84ac8ad6107f49144a40e0e831d5e8670098d3ad
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2018-06-06 20:51:16 +0200

    parametrize in the set of distinctions

--- Lib/Distinction.hs
+++ Lib/Distinction.hs
@@ -32,7 +32,7 @@ import Uniform.Error
 
 import Belnap
 
-
+-- | the distinctions in the paper (reduced)
 data DistPaper = PhysObj | Human | Liquid | Edible | Tool
 --            | Dairy | Cuttlery | Fermented
                 deriving (Show, Read, Eq, Ord, Enum, Bounded)
@@ -42,13 +42,13 @@ type Distinction = DistPaper
 instance Arbitrary DistPaper where
     arbitrary = arbitraryBoundedEnum
 
-data DistValue = DV {d :: DistPaper, v:: B4val} -- | DVtop | DVbot
+data DistValue dist = DV {d :: dist, v:: B4val} -- | DVtop | DVbot
             deriving (Show, Read, Eq, Ord)
             -- only True4 and Fals4 should occur, rest is always mapped to DVtop or DVbot?
 
 ----instance Show DistValue where
-showDV (DV d v) = -- if isTop v then "" else
-                            (show d) ++ s
+showDV :: Show d => DistValue d -> Text
+showDV (DV d v) = s2t $  (show d) ++ s
         where s = case v of
                     True4 -> "+"
                     False4 -> "-"
@@ -56,7 +56,7 @@ showDV (DV d v) = -- if isTop v then "" else
                     None4 -> "*"
 
 
-instance Arbitrary DistValue where
+instance Arbitrary d => Arbitrary (DistValue d) where
     arbitrary =   fmap normalize $ fmap pair2dv arbitrary  -- (arbitrary, arbitrary)
 
 -- these are raw distinction values, not taxa
@@ -64,8 +64,8 @@ physObj' = DV PhysObj True4
 human' = DV Human True4
 stuff' = DV Human False4
 liquid' = DV Liquid True4
-top' = top :: DistValue
-bot' = bottom :: DistValue
+top' = top :: DistValuePaper
+bot' = bottom :: DistValuePaper
 
 test_join1 = assertEqual physObj' (ljoin physObj' physObj')
 test_join2 = assertEqual (DV{d = Human, v = None4}) (ljoin human' stuff')
@@ -79,39 +79,22 @@ pair2dv (d,v) = DV d v
 --pair2dv (minBound, Both4) = DVbot
 --pair2dv a = errorT ["pair2dv for", showT a]
 
-prop_inv1 :: DistValue -> Bool
+prop_inv1 :: DistValuePaper -> Bool
 prop_inv1 a = a == (pair2dv . dv2pair $ a)
 
 prop_inv2 :: (Distinction,B4val) -> Bool
 prop_inv2 a = a == (dv2pair . pair2dv $ a)
 
-instance Lattice DistValue where
---    lcompare (DV d1 v1) (DV d2 v2) =
+instance (Eq d, Bounded d) => Lattice (DistValue d) where
     lcompare2 a@(DV d1 v1) b@(DV d2 v2) = if d1 == d2 then lcompare v1 v2 else INC
---    lcompare DVbot DVbot = PEQ
---    lcompare DVtop _ = PGT
---    lcompare _ DVtop = PLT
---    lcompare DVbot _ = PLT
---    lcompare _ DVbot = PGT
     lmeet2  (DV d1 v1)  (DV d2 v2) = if d1 == d2 then normalize(DV d1 (lmeet v1 v2)) else bottom
---    lmeet DVtop a = a
---    lmeet a DVtop = a
---    lmeet DVbot a = bottom
---    lmeet a DVbot = bottom
---    lmeet x y = errorT ["Lattice DistValue lmeet", showT x, showT y]
     ljoin2  (DV d1 v1)  (DV d2 v2) = if d1 == d2 then normalize(DV d1 (ljoin v1 v2)) else top
---        if d1 == d2 then normalize(DV d1 (ljoin v1 v2)) else top
---    ljoin a DVbot = a
---    ljoin DVbot a = a
---    ljoin a DVtop = top
---    ljoin DVtop a = top
---    ljoin x y = errorT ["Lattice DistValue ljoin", showT x, showT y]
     top = pair2dv (minBound, None4)  -- alternative DVtop
     bottom = pair2dv (minBound, Both4)   -- DVbot
 
 normalize a@(DV d v) = a -- if v==bottom then bottom else if v==top then top else a
 
-instance LatticeTests DistValue
+instance (Eq d, Bounded d) =>  LatticeTests (DistValue d)
 
 test_1 = assertEqual (lsub human' human')  False
 test_2 = assertEqual (lsub human' top') True
@@ -124,24 +107,26 @@ test_6 = assertEqual (lmeet human' liquid') bottom
 test_7 = assertEqual (ljoin human' liquid') top
 --    prop_ass1 a b c = lmeet a (lmeet b c) ==  lmeet(lmeet a b)  c
 
-prop_compSymx :: DistValue -> DistValue -> Bool
+type DistValuePaper = DistValue DistPaper
+
+prop_compSymx :: DistValuePaper -> DistValuePaper -> Bool
 prop_compSymx = prop_compSym -- lcompare a b == reverse4 (lcompare b a)
 
-prop_ide1x, prop_ide2x :: DistValue -> Bool
+prop_ide1x, prop_ide2x :: DistValuePaper -> Bool
 prop_ide1x = prop_ide1
 prop_ide2x = prop_ide2
 --
-prop_id1x, prop_id2x :: DistValue -> Bool
+prop_id1x, prop_id2x :: DistValuePaper -> Bool
 prop_id1x = prop_id1  -- ljoin a a == a
 prop_id2x = prop_id2
 --
 test_9 = assertEqual (ljoin liquid' liquid') liquid'
 
-prop_ass1x, prop_ass2x :: DistValue -> DistValue -> DistValue -> Bool
+prop_ass1x, prop_ass2x :: DistValuePaper -> DistValuePaper -> DistValuePaper -> Bool
 prop_ass1x = prop_ass1 -- lmeet a (lmeet b c) ==  lmeet(lmeet a b)  c
 prop_ass2x = prop_ass2 -- ljoin a (ljoin b c) == ljoin (ljoin a b)  c
 
-prop_abs1x, prop_abs2x :: DistValue -> DistValue ->  Bool
+prop_abs1x, prop_abs2x :: DistValuePaper -> DistValuePaper ->  Bool
 prop_abs1x = prop_abs1
 prop_abs2x = prop_abs2
 
--- Lib/Taxon.hs
+++ Lib/Taxon.hs
@@ -25,29 +25,31 @@ import Distinction
 import qualified Data.Map as Map
 import Data.List
 
-newtype Taxon = Taxon (Map.Map Distinction B4val)  deriving (Show, Read, Eq)
-unTaxon (Taxon m) = m
+newtype Taxon d = Taxon (Map.Map d B4val)  deriving (Show, Read, Eq)
+-- | taxon over a set of distinctions d
 
-class Taxons t where
-    specialize :: Distinction -> B4val -> t -> t
-    showTaxon :: t -> Text
+class (Show d) => Taxons t d where
+    specialize :: d -> B4val -> t d -> t d
+    specializeList :: [(d, B4val)] -> t d -> t d
+    showTaxon :: t d -> Text
+    unTaxon :: t d -> (Map.Map d B4val)
 
+    dvList2taxon :: [DistValue d] -> t d
+    taxon2dvList :: t d -> [DistValue d]
 
-instance Taxons Taxon where
-    specialize d v t = Taxon . Map.insert d v . unTaxon $ t
-    showTaxon t =  s2t . unwords . map showDV
+instance  (Show d, Eq d, Ord d) => Taxons Taxon d where
+    specialize d v t = Taxon  . Map.insert d v . unTaxon $ t
+    showTaxon t =   unwords' . map showDV
                                 . taxon2dvList  $ t
+    unTaxon (Taxon m) = m
 
-specializeList :: [(Distinction, B4val)] -> Taxon -> Taxon
-specializeList l t1= foldl (\t (d,v) -> specialize d v t) t1  l
+    specializeList l t1= foldl (\t (d,v) -> specialize d v t) t1  l
 
-dvList2taxon :: [DistValue] -> Taxon
-dvList2taxon   =  Taxon . Map.fromList .   (map  dv2pair)
+--    dvList2taxon :: [DistValue d] -> Taxon d
+    dvList2taxon   =  Taxon  . Map.fromList .   (map  dv2pair)
+    taxon2dvList   =        map pair2dv . Map.toAscList . unTaxon
 
-taxon2dvList :: Taxon -> [DistValue]
-taxon2dvList   =        map pair2dv . Map.toAscList . unTaxon
-
-instance Lattice Taxon where
+instance (Eq d, Bounded d, Enum d, Show d, Ord d) => Lattice (Taxon d) where
     lcompare = compareTaxon
     lmeet = meetTaxon
     ljoin = joinTaxon
@@ -56,25 +58,26 @@ instance Lattice Taxon where
     isBottom = any (isBottom . v) . taxon2dvList
     isTop = all (isTop . v) . taxon2dvList
 
-makeAll v = map (\d -> DV d v) allDist :: [DistValue]
+makeAll :: (Enum d, Bounded d) => B4val -> [DistValue d]
+makeAll v = map (\d -> DV d v) allDist -- :: [DistValue d]
     where
-            allDist = [minBound .. maxBound] :: [DistPaper]
+            allDist = [minBound .. maxBound] -- :: [DistPaper]
 
-joinTaxon :: Taxon -> Taxon -> Taxon
+joinTaxon :: (Eq d, Bounded d, Show d, Ord d) => Taxon d -> Taxon d -> Taxon d
 joinTaxon a b = dvList2taxon l
     where
-        aa = taxon2dvList a :: [DistValue]
-        bb = taxon2dvList b :: [DistValue]
-        l = zipWith ljoin aa bb :: [DistValue]
+        aa = taxon2dvList a -- :: [DistValue d]
+        bb = taxon2dvList b -- :: [DistValue d]
+        l = zipWith ljoin aa bb -- :: [DistValue d]
 
-meetTaxon :: Taxon -> Taxon -> Taxon
+meetTaxon :: (Eq d, Bounded d, Show d, Ord d) => Taxon d -> Taxon d -> Taxon d
 meetTaxon a b = dvList2taxon l
     where
-        aa = taxon2dvList a :: [DistValue]
-        bb = taxon2dvList b :: [DistValue]
-        l = zipWith lmeet aa bb :: [DistValue]
+        aa = taxon2dvList a -- :: [DistValue d]
+        bb = taxon2dvList b -- :: [DistValue d]
+        l = zipWith lmeet aa bb -- :: [DistValue d]
 
-compareTaxon :: Taxon -> Taxon -> PartialRel
+compareTaxon :: (Eq d, Bounded d, Show d, Ord d) => Taxon d -> Taxon d -> PartialRel
 compareTaxon a b = if a==b then PEQ
                     else if a==l then PGT
                     else if b==l then PLT
@@ -85,7 +88,7 @@ ph = taxon2dvList physObj
 t = zipWith ljoin ph ph
 test_x = assertEqual ph t
 
-
+type TaxonPaper = Taxon DistPaper
 
 test_join1 = assertEqual physObj (joinTaxon physObj physObj)
 test_join_milk = assertEqual someFood (joinTaxon milk bread)
@@ -93,33 +96,33 @@ test_join_milk = assertEqual someFood (joinTaxon milk bread)
     --   (Edible, True4), (Tool, None4)]
 someFood = specializeList [(PhysObj, True4), (Human, False4), (Liquid, None4),
               (Edible, True4), (Tool, None4)] ttop
-prop_comm1x :: Taxon -> Taxon -> Bool
+prop_comm1x :: TaxonPaper -> TaxonPaper -> Bool
 prop_comm1x   = prop_comm1
-prop_comm2x :: Taxon -> Taxon -> Bool
+prop_comm2x :: TaxonPaper -> TaxonPaper -> Bool
 prop_comm2x = prop_comm2
 
-prop_ass1x, prop_ass2x :: Taxon -> Taxon -> Taxon -> Bool
+prop_ass1x, prop_ass2x :: TaxonPaper -> TaxonPaper -> TaxonPaper -> Bool
 prop_ass1x = prop_ass1
 prop_ass2x = prop_ass2
 --
-prop_abs1x, prop_abs2x :: Taxon -> Taxon ->  Bool
+prop_abs1x, prop_abs2x :: TaxonPaper -> TaxonPaper ->  Bool
 prop_abs1x = prop_abs1
 prop_abs2x = prop_abs2
 --
-prop_id1x, prop_id2x :: Taxon -> Bool
+prop_id1x, prop_id2x :: TaxonPaper -> Bool
 prop_id1x = prop_id1
 prop_id2x = prop_id2
 --
-prop_ide1x, prop_ide2x :: Taxon -> Bool
+prop_ide1x, prop_ide2x :: TaxonPaper -> Bool
 prop_ide1x = prop_ide1
 prop_ide2x = prop_ide2
 --
-prop_compSymx :: Taxon -> Taxon -> Bool
+prop_compSymx :: TaxonPaper -> TaxonPaper -> Bool
 prop_compSymx = prop_compSym
 
 
-tbottom = bottom :: Taxon
-ttop = top :: Taxon
+tbottom = bottom :: TaxonPaper
+ttop = top :: TaxonPaper
 
 test_top = assertEqual "PhysObj* Human* Liquid* Edible* Tool*" (showTaxon ttop)
 test_bottom = assertEqual "PhysObjX HumanX LiquidX EdibleX ToolX" (showTaxon tbottom)
@@ -142,9 +145,12 @@ solid = specialize Liquid False4 stuff
 test_0 = assertEqual  "PhysObj+ Human* Liquid* Edible* Tool*" (showTaxon physObj)
 test_1 = assertEqual  "PhysObj+ Human+ Liquid* Edible* Tool*" (showTaxon human)
 test_2 = assertEqual  "PhysObj+ Human- Liquid* Edible* Tool*" (showTaxon stuff)
-test_edible = assertEqual "PhysObj+ Human- Liquid* Edible+ Tool*" (showTaxon (edible::Taxon))
-test_milk = assertEqual "PhysObj+ Human- Liquid+ Edible+ Tool*" (showTaxon (milk::Taxon))
-test_liquid = assertEqual "PhysObj+ Human- Liquid+ Edible* Tool*" (showTaxon (liquid::Taxon))
+test_edible = assertEqual "PhysObj+ Human- Liquid* Edible+ Tool*"
+            (showTaxon (edible::Taxon DistPaper))
+test_milk = assertEqual "PhysObj+ Human- Liquid+ Edible+ Tool*"
+            (showTaxon (milk::Taxon DistPaper))
+test_liquid = assertEqual "PhysObj+ Human- Liquid+ Edible* Tool*"
+            (showTaxon (liquid::Taxon DistPaper))
 
 test_PEQ1 = assertEqual PEQ (lcompare physObj physObj)
 test_PEQ2 = assertEqual INC (lcompare human stuff)
@@ -154,12 +160,12 @@ test_PEQ5 = assertEqual INC (lcompare human edible)
 test_PEQ6 = assertEqual PLT (lcompare milk edible)
 test_PEQ7 = assertEqual PGT (lcompare liquid milk)
 
-instance LatticeTests Taxon
+instance (Eq d, Enum d, Bounded d, Show d, Ord d) => LatticeTests (Taxon d)
 
 
-instance Arbitrary Taxon where
+instance (Eq d, Enum d, Bounded d, Show d, Ord d, Arbitrary d) =>Arbitrary (Taxon d) where
     arbitrary = do
-        d :: [DistValue] <- arbitrary
+        d :: [DistValue d] <- arbitrary
         let d2 = makeAll None4 ++ d
         return $ dvList2taxon d2
 
